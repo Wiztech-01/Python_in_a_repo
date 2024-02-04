c63149b8d5a6c75fd6e4a859a82845e38f896346
festus-sulumeti <sulumetifestus@outlook.com>
2024-02-04 11:22:40 +0300

    python in a repo

---
+++ 02type_system/04stringformatting.py
@@ -0,0 +1,14 @@
+# String formatting
+# Using the f-string
+# The f-string is a string that is prefixed with the letter f or F. Characters in the string are treated as they are except for the curly braces {}
+# which are used to evaluate the expression inside them.
+# The f-string is used to format strings in python. It is a new way to format strings in python. It is more readable and less error-prone.
+
+
+userName = "festus"
+userage = 13
+userdog = "Rex"
+
+print(f"Hello {userName}! How are you today?")
+print(f"Your age is {userage} years old")
+print(f"Your dog's name is {userdog}")
