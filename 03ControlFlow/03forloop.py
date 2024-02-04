d045b3cb393fe71e1a6250c88e18c1ff78a2f20b
festus-sulumeti <sulumetifestus@outlook.com>
2024-02-04 20:12:51 +0300

    python in a repo

---
+++ 03ControlFlow/03forloop.py
@@ -0,0 +1,15 @@
+"""
+  Iterating in the for loop
+    The for loop is used to iterate over a sequence (list, tuple, string) or other iterable objects.
+    Iterating over a sequence is called traversal.
+    The for loop in Python is used to iterate over a sequence (list, tuple, string) or other iterable objects.
+    The for loop is used to execute a block of code multiple times.
+   Each iteration will be printing the character of the username on new lines
+"""
+
+username: str = "Festus"
+
+
+for character in username:
+    print(character)
+
