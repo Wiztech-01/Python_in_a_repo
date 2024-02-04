9c5ccb1d741342e55598792e66ca7e3ab2930bd2
festus-sulumeti <sulumetifestus@outlook.com>
2024-02-04 19:31:51 +0300

    python in a repo

--- 03ControlFlow/and_or.py
+++ 03ControlFlow/and_or.py
@@ -26,14 +26,47 @@
     not False = True
     
    
-   
+   A good way to learn this is by installing the ipython by using the following commands
+    1. pip install ipython
+    
+    - once installled go to the terminal and type the following command
+    1. ipython
+    
+    - from there you can now test the following commands
+    1. True and True
+    2. True and False
+    3. False and True
+    4. False and False
+    5. True or True
+    6. True or False
+    7. False or True
+    8. False or False
+    9. not True
+    10. not False
+    11. not True and False
+    12. not True or False
+    13. not True and not False
+    14. not True or not False
+    15. not True and not True
+    16. not True or not True
+    17. not False and not False
+    18. not False or not False
+    19. not False and not True
+    20. not False or not True
+    21. not True and not True
+    22. not True or not True
+    23. not True and not False
+    24. not True or not False
+    25. not False and not False
+  
+   This will help you understand the and, or and not operators, especially when you are not sure on which one to use    
 """
 
 userage: int = 17
 country: str = "Kenya"
 
 """
-   my first and and or operator
+   my first and operator
    
 """
 
