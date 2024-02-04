3424a39c2cac1c6f41ed656a7ce3b30c4f4eb124
festus-sulumeti <sulumetifestus@outlook.com>
2024-02-04 11:17:22 +0300

    python in a repo

---
+++ 02type_system/01variables.py
@@ -0,0 +1,32 @@
+# Variables are used to store information to be referenced and manipulated in a computer program.
+# They also provide a way of labeling data with a descriptive name, so our programs can be understood more clearly by the reader and ourselves. 
+# It is helpful to think of variables as containers that hold information. 
+# Their sole purpose is to label and store data in memory. This data can then be used throughout your program.
+
+
+# The variable box now holds the value "balloons"
+box = "balloons"
+
+
+# The value of the variable box is printed to the console.
+print(box)
+
+
+# The value of the variable box is changed to "biscuits"
+box = "biscuits"
+
+# The value of the variable box is printed to the console.
+print(box)
+
+
+# The value of the variable box is changed to "hats"
+box = "hats"
+
+# The value of the variable box is printed to the console.
+print(box)
+
+
+# From the above code, we can see that the variable box is being used to store different values.
+# The value of the variable box is changed multiple times.
+# The value of the variable box is printed to the console after each change.
+# The variable box is used to store different values at different times.
--- 02type_system/03primitivedatatypes.py
+++ 02type_system/03primitivedatatypes.py
@@ -4,9 +4,15 @@
    The following are the primitive data types in Python
    
    1. Strings = "Festus"     in strings,  a valid sting can have either double or single quote. also ensure the consistency when using either of the quote
+               
+                 
    2. Integers = 45     majorly they are only whole numbers from 0 - 9
+                 - In python, the integer can be positive or negative numbers
+                  
    3. Floats = 3.14     All the decimal point number that you can think of 
+               - In python, the decimal point is represented by a dot (.)
    4. Boolean = True/False  it can also be stated as a yes or no, 1/0 
+               - Remember in python, the first letter of the boolean value must be in capital letter
 """
 
 my_string = 'Festus'
